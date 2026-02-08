2e7536c27f04878b263f92eb449e57be6d991b3e
banana-three-join <lenoxwiltshire@gmail.com>
2026-02-08 15:46:07 -0600

    removed magic numbers

--- utils.go
+++ utils.go
@@ -7,20 +7,21 @@ import (
 )
 
 func Serialize(src Row, dst []byte) {
-	binary.LittleEndian.PutUint32(dst[0:4], src.Id)
-	copy(dst[4:36], src.Username)
-	copy(dst[36:291], src.Email)
+	binary.LittleEndian.PutUint32(dst[:IdSize], src.Id)
+	copy(dst[UsernameOffset:UsernameOffset+UsernameSize], src.Username)
+	copy(dst[EmailOffset:EmailOffset+EmailSize], src.Email)
 }
 
+// deserializes only one row. maybe implement a deserialize for a full page?
 func Deserialize(src []byte, dst *Row) error {
 
 	if len(src) < RowSize {
 		return fmt.Errorf("buffer too small to contain a Row")
 	}
 
-	dst.Id = binary.LittleEndian.Uint32(src[0:4])
-	dst.Username = string(bytes.Trim(src[4:36], "\x00"))
-	dst.Email = string(bytes.Trim(src[36:291], "\x00"))
+	dst.Id = binary.LittleEndian.Uint32(src[:IdSize])
+	dst.Username = string(bytes.Trim(src[UsernameOffset:UsernameOffset+UsernameSize], "\x00"))
+	dst.Email = string(bytes.Trim(src[EmailOffset:EmailOffset+EmailSize], "\x00"))
 
 	return nil
 }
